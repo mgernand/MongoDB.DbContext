24feab3f2a654a6798e32b4bfaab383e800fcc82
Matthias Gernand <mgernand@users.noreply.github.com>
2024-02-22 18:32:09 +0100

    Added System.Diagnostics extensions for the MongoDB driver. (#14)

--- src/MongoDB.DbContext/MongoDbContextOptionsBuilderExtensions.cs
+++ src/MongoDB.DbContext/MongoDbContextOptionsBuilderExtensions.cs
@@ -1,6 +1,5 @@
 ﻿namespace MadEyeMatt.MongoDB.DbContext
 {
-	using System;
 	using JetBrains.Annotations;
 
 	/// <summary>
@@ -16,13 +15,47 @@
 		/// <param name="connectionString"></param>
 		/// <param name="databaseName"></param>
 		/// <returns></returns>
-		public static MongoDbContextOptionsBuilder UseDatabase(this MongoDbContextOptionsBuilder builder, string connectionString, string databaseName)
+		public static MongoDbContextOptionsBuilder UseDatabase(this MongoDbContextOptionsBuilder builder, 
+			string connectionString, string databaseName)
 		{
+			Argument.ThrowIfNull(builder);
 			Argument.ThrowIfNullOrEmpty(connectionString);
 			Argument.ThrowIfNullOrEmpty(databaseName);
 
 			builder.Options.ConnectionString = connectionString;
 			builder.Options.DatabaseName = databaseName;
+
+			return builder;
+		}
+
+		/// <summary>
+		///		Sets a flag, indicating that the telemetry/diagnostics are enabled for the MongoDB driver.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <param name="enableTelemetry"></param>
+		/// <returns></returns>
+		public static MongoDbContextOptionsBuilder EnableTelemetry(this MongoDbContextOptionsBuilder builder,
+			bool enableTelemetry = true)
+		{
+			Argument.ThrowIfNull(builder);
+
+			builder.Options.EnableTelemetry = enableTelemetry;
+
+			return builder;
+		}
+
+		/// <summary>
+		///		Sets a flag, indicating that the command text should be part of the telemetry.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <param name="captureCommandText"></param>
+		/// <returns></returns>
+		public static MongoDbContextOptionsBuilder CaptureCommandText(this MongoDbContextOptionsBuilder builder, 
+			bool captureCommandText = true)
+		{
+			Argument.ThrowIfNull(builder);
+
+			builder.Options.CaptureCommandText = captureCommandText;
 
 			return builder;
 		}
